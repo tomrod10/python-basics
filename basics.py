fab23901956f8b89b284e1fbc2d2f16a3d79df3d
tomrod10 <tomrod243@gmail.com>
2023-09-02 00:05:09 -0700

    more data structures and f string

--- basics.py
+++ basics.py
@@ -39,9 +39,31 @@ rgb_color = (255, 0, 0)
 
 # Sets - are unordered collections of unique elements. 
 # They are useful for tasks that require handling distinct values.
-``
+unique_numbers = {1, 2, 3, 4 , 5}
+
+# Arrays - similar to lists, but they are more efficient 
+# for numerical computations when using the numpy library. 
+# They provide powerful mathematical operations.
+# import numpy as np
+# num_array = np.array([1, 2, 3, 4, 5])
+
+# Stacks and queues are abstract data types.
+# Example using collections module:
+from collections import deque
+stack = deque()
+queue = deque()
+
+# Python's standard library and third-party packages offer more specialized 
+# data structures like linked lists, heaps, trees, and graphs, among others.
 
 # Control flow
+# Python executes like a person reads a book:
+# Left to right, top to bottom.
+
+# f string - equivalent to interpolated string in JavaScript
+age = 34
+print(f"My age is {age}")
+
 
 # Functions
 
