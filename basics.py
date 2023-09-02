dee1b0c9a75629f745c5cd00f6dd36447a04d4fd
tomrod10 <tomrod243@gmail.com>
2023-09-01 23:51:29 -0700

    data types and data structures

---
+++ basics.py
@@ -0,0 +1,67 @@
+# Data types
+
+# string
+'hello'
+
+# int
+num = 4
+
+# float
+decimal = 1.52
+
+# complex
+z = 2 + 3j
+
+# boolean
+is_valid = True
+is_sad = False
+
+# None - represents the absence of a value or a null value.
+None
+result = None
+
+#----------
+
+# Data Structures
+
+# Dictionary - represents collections of key-value pairs.
+person = {"name": "John", "age": 30}
+
+# Lists - are ordered collections of items, which can 
+# be of different types. They are mutable.
+numbers = [1, 2, 3, 4, 5]
+fruits = ["apple", "banana", "pear"]
+mixed_list = [1, "mango", 1.5, False]
+
+# Tuples - similar to lists but are immutable
+point = (3, 4)
+rgb_color = (255, 0, 0)
+
+# Sets - are unordered collections of unique elements. 
+# They are useful for tasks that require handling distinct values.
+``
+
+# Control flow
+
+# Functions
+
+# String Manipulation
+
+# File Handling
+
+# Error Handling
+
+# OOP
+
+# Modules and Libraries
+
+# Adv Topics
+    # list comprehensions,
+    # lambda functions,
+    # decorators, 
+    # generators
+    # APIs
+    # Dates
+    # Database Interaction
+
+# 
