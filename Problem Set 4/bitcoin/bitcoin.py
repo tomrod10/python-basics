f69ab798e353c8d9081ea0c14e8f7de86f8cd23f
tomrod10 <tomrod243@gmail.com>
2023-12-04 15:09:43 -0800

    bitcoin problem done

---
+++ Problem Set 4/bitcoin/bitcoin.py
@@ -0,0 +1,23 @@
+import sys
+import requests
+
+def main():
+    get_bitcoin_in_USD()
+
+def get_bitcoin_in_USD():
+    try:
+        if len(sys.argv) < 2:
+            sys.exit("Missing command-line argument")
+        amount = float(sys.argv[1])
+    except ValueError:
+        sys.exit("Command-line argument is not a number")
+    try:
+        bitcoin_data = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
+    except requests.RequestException:
+        sys.exit("There's an error processing your request. Try again later.")
+    bitcoin_rate = bitcoin_data["bpi"]["USD"]["rate_float"]
+    amount_cost = bitcoin_rate * amount
+    print(f"${amount_cost:,.4f}")
+    sys.exit()
+
+main()
