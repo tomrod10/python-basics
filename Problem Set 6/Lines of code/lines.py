6f9d6f26f6eaf306fd63d399f50cb701baa2e870
tomrod10 <tomrod243@gmail.com>
2024-01-03 12:34:45 -0400

    define main and count code lines function

--- Problem Set 6/Lines of code/lines.py
+++ Problem Set 6/Lines of code/lines.py
@@ -1,18 +1,27 @@
 import sys
 
-try:
-    if len(sys.argv) < 2:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
-    elif not sys.argv[1].endswith(".py"):
-        sys.exit("Not a python file")
 
-    lines = 0
-    with open(sys.argv[1]) as file:
-        for line in file:
-            if not line.lstrip().startswith("#") and line.lstrip() != "":
-                lines += 1
-    print(lines)
-except FileNotFoundError:
-    sys.exit("File does not exist")
+def main():
+    count_code_lines()
+
+
+def count_code_lines():
+    try:
+        if len(sys.argv) < 2:
+            sys.exit("Too few command-line arguments")
+        elif len(sys.argv) > 2:
+            sys.exit("Too many command-line arguments")
+        elif not sys.argv[1].endswith(".py"):
+            sys.exit("Not a python file")
+
+        lines = 0
+        with open(sys.argv[1]) as file:
+            for line in file:
+                if not line.lstrip().startswith("#") and line.lstrip() != "":
+                    lines += 1
+        print(lines)
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+
+main()
