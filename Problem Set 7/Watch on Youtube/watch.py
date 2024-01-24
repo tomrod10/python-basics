efc8ebd9326a3ab8509e3af0fb382e85fb2f1b83
tomrod10 <tomrod243@gmail.com>
2024-01-24 15:25:46 -0800

    cleanup code

--- Problem Set 7/Watch on Youtube/watch.py
+++ Problem Set 7/Watch on Youtube/watch.py
@@ -7,13 +7,7 @@ def main():
 
 # <iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
 def parse(html):
-    src_attr = re.search(r"(\bsrc=)(.+\")", html)
-    # print(src_attr)
-    url = src_attr.group(2)
-    # From: ---> "https://www.youtube.com/embed/xvFZjo5PgG0"
-    # To: ---> https://youtu.be/xvFZjo5PgG0
-
-    # grab to the right of embed/{video_id}
+    url = re.search(r"(?:\bsrc=)(.+\")", html).group(1)
     video_id = re.search(r"(?:embed/)(.+[a-zA-Z0-9])", url).group(1)
     shortened_url = "https://youtu.be/" + video_id
     return shortened_url
