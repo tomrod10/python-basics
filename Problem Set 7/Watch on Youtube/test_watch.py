0da5f37284b6d4df5d51cac021eb1c891def39d7
tomrod10 <tomrod243@gmail.com>
2024-01-24 17:15:54 -0800

    fix test names

--- Problem Set 7/Watch on Youtube/test_watch.py
+++ Problem Set 7/Watch on Youtube/test_watch.py
@@ -2,11 +2,11 @@ from watch import parse
 
 
 def main():
-    valid_html()
-    invalid_html()
+    test_valid_html()
+    test_invalid_html()
 
 
-def valid_html():
+def test_valid_html():
     assert (
         parse('<iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>')
         == "https://youtu.be/xvFZjo5PgG0"
@@ -19,7 +19,7 @@ def valid_html():
     )
 
 
-def invalid_html():
+def test_invalid_html():
     assert (parse("dog")) == None
     assert (
         parse('<iframe width="560" src="https://cs50.harvard.edu/python"></iframe>')
