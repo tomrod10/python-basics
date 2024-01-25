a6a1f2187570f53f9901ec5abfd618971a250c9b
tomrod10 <tomrod243@gmail.com>
2024-01-24 17:12:50 -0800

    add tests

--- Problem Set 7/Watch on Youtube/watch.py
+++ Problem Set 7/Watch on Youtube/watch.py
@@ -5,17 +5,14 @@ def main():
     print(parse(input("HTML: ")))
 
 
-# <iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
-
-
-# <iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player"
-# frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
-# picture-in-picture" allowfullscreen></iframe>
 def parse(html):
-    url = re.search(r"(?:\bsrc=)(.+?\")", html).group(1)
-    video_id = re.search(r"(?:embed/)(.+[a-zA-Z0-9])", url).group(1)
-    shortened_url = "https://youtu.be/" + video_id
-    return shortened_url
+    try:
+        url = re.search(r"(?:\bsrc=)(.+?\")", html).group(1)
+        video_id = re.search(r"(?:embed/)(.+[a-zA-Z0-9])", url).group(1)
+        shortened_url = "https://youtu.be/" + video_id
+        return shortened_url
+    except AttributeError:
+        return None
 
 
 if __name__ == "__main__":
