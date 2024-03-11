21a3d58e8a58af5c0030b4d99c92d5b1f2563806
tomrod10 <tomrod243@gmail.com>
2024-03-11 16:09:08 -0700

    cookie-jar

---
+++ Problem Set 8/cookie-jar/test_jar.py
@@ -0,0 +1,34 @@
+import pytest
+from jar import Jar
+
+jar = Jar()
+
+def test_init():
+    assert str(jar) == ""
+    assert jar.capacity == 12
+    assert jar.size == 0
+
+def test_deposit():
+    assert jar.capacity == 12
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(2)
+    assert str(jar) == "🍪🍪🍪"
+    assert jar.size == 3
+
+def test_withdraw():
+    jar.withdraw(2)
+    assert str(jar) == "🍪"
+    assert jar.size == 1
+    jar.withdraw(1)
+    assert str(jar) == ""
+    assert jar.size == 0
+
+def test_exceptions():
+    with pytest.raises(ValueError):
+        jar.deposit(20)
+    with pytest.raises(ValueError):
+        jar.withdraw(20)
+    with pytest.raises(ValueError):
+        Jar(-3)
+
